c29e4043173188bb654b4fa6f067ae4d6a17c61e
Elijah Zimmerman <elijahz0409@gmail.com>
2026-05-07 12:47:09 -0400

    Outline ChunkedBuffer struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,48 @@
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod chunked_buffer {
+    //! A growable vector which stores data in chunks.
+    //!
+    //! Space is not reserved for null values.
+    //! Primarily useful when data chunks are spread widely through the container.
+    //! Uses [`num_bigint::BigUint`] for indexing outside the range of an [`u128`].
+    //!
+    //! This should be considered infinitely long, limited only by ram space.
+    //! So, no index out-of-bounds error may occur from this data structure.
+    //! Indexes beyond the last chunk of data simply return the null value assigned at initialization.
+    //!
+    //! **Note:** [`u128`] indexing fails automatically if the buffer's max address exceeds the [`u128`] max.
+    //! As such, it is generally recommended to use [`BigUint`][`num_bigint::BigUint`] wherever possible.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    use std::collections::BTreeMap;
+    use std::ops::Index;
+    use num_bigint::BigInt;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// A growable vector which stores data in chunks.
+    pub struct ChunkedBuffer<T> {
+        chunks: BTreeMap<BigInt, Box<[T]>>,
+        null_value: T,
+    }
+
+    impl<T> ChunkedBuffer<T> {
+
+    }
+
+    /// Dumb indexing.
+    ///
+    /// Returns null value if no data at index, or index out-of-bounds.
+    impl<T, Q: Into<BigInt>> Index<&Q> for ChunkedBuffer<T> {
+        type Output = T;
+
+        fn index(&self, index: &Q) -> &Self::Output {
+            todo!()
+        }
+    }
+
+    impl <T> Index<u128> for ChunkedBuffer<T> {
+        type Output = Option<T>;
+
+        fn index(&self, index: u128) -> &Self::Output {
+            todo!()
+        }
     }
 }
